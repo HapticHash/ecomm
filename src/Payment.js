52a48ee67a9eb7f28a3bedda3955ea6fdc2f0e97
haptichash <harshitks2203@gmail.com>
2020-11-14 16:50:09 -0500

    added cloud functions

--- src/Payment.js
+++ src/Payment.js
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Checkout from './Checkout';
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
 import { useStateValue } from './StateProvider'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
+import CurrencyFormat from 'react-currency-format'
+import { getBasketTotal } from "./reducer"
+import axios from './axios'
 
 function Payment() {
     
     const [{ basket, user }, dispatch ] = useStateValue();
+    const history = useHistory()
 
+    const stripe = useStripe()
+    const elements = useElements()
+
+    const [succeeded, setSucceeded] = useState(false)
+    const [processing, setProcessing] = useState("")
+
+    const [error, setError] = useState(null)
+    const [disabled, setDisabled] = useState(true)
+    const [clientSecret, setClientSecret] = useState(true)
+
+    useEffect(() => {
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: 'post',
+                url: `/payment/create?total=${getBasketTotal(basket) * 100}`
+            })
+            setClientSecret(response.data.clientSecret)
+        }
+        getClientSecret()
+    }, [basket])
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        setProcessing(true)
+
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement)
+            }
+        }).then(({ paymentIntent }) => {
+            setSucceeded(true)
+            setError(null)
+            setProcessing(false)
+
+            history.replace('/orders')
+        })
+    }
+
+    const handleChange = event => {
+        setDisabled(event.empty)
+        setError(event.error ? event.error.message: "")
+    }
     return (
         <div className="payment">
             <div className="payment__container">
@@ -49,6 +96,28 @@ function Payment() {
                 <div className="payment__section">
                     <div className="payment__title">
                         <h3> Payment Method</h3>
+                    </div>
+                    <div className="payment__details">
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange}/>
+
+                            <div className="payment__priceContainer">
+                            <CurrencyFormat
+                                renderText={(value) => (
+                                    <h3>Order total: {value} </h3>
+                                )}
+                                decimalScale={2}
+                                value={getBasketTotal(basket)}
+                                displayType={"text"}
+                                thousandSeparator={true}
+                                prefix={"$"}
+                            />
+                            <button disabled={processing || disabled || succeeded }>
+                                <span> {processing ? <p>Processing</p> : "Buy Now"} </span>
+                            </button>
+                            </div>
+                            {error && <div>{error}</div>}
+                        </form>
                     </div>
                 </div>
             </div>
