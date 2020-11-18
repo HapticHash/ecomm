9d14a49f5e4697c649e30759a2b04e9444062f28
haptichash <harshitks2203@gmail.com>
2020-11-17 19:19:34 -0500

    deployed cloud functions

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/ecomm-7c2ae/us-central1/api'
+    baseURL: 'https://us-central1-ecomm-7c2ae.cloudfunctions.net/api' 
+    //'http://localhost:5001/ecomm-7c2ae/us-central1/api'
 })
 
 export default instance
